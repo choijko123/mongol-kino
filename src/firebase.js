2a7b176ba8a98f12a139fb6aacba7b2f7b0c804d
choijko123 <kunonameol5@gmail.com>
2026-04-02 22:16:27 +0800

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, startAfter } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCuH0nJx7f0e8KssPo8rFxc47IT50uXl_0",
   authDomain: "movie-time-92adb.firebaseapp.com",
   projectId: "movie-time-92adb",
-  storageBucket: "movie-time-92adb.firebasestorage.app",
+  storageBucket: "movie-time-92adb.appspot.com",
   messagingSenderId: "92887648203",
-  appId: "1:92887648203:web:aeabc9773e35f810a0984e",
-  measurementId: "G-FS51Q7CV9V"
+  appId: "1:92887648203:web:823c50c332414460a0984e"
 };
 
 
